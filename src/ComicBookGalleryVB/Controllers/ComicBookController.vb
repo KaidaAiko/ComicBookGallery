8e2d879384e1d377968690fc0af1505eadf5c7ab
KaidaAiko <42393932+KaidaAiko@users.noreply.github.com>
2019-03-02 21:03:13 -0600

    Proposed Cleanup

--- src/ComicBookGalleryVB/Controllers/ComicBookController.vb
+++ src/ComicBookGalleryVB/Controllers/ComicBookController.vb
@@ -3,23 +3,21 @@ Imports ComicBookGalleryVB.Models
 Imports ComicBookGalleryVB.Data
 
 Namespace Controllers
-    Public Class ComicBookController
-        Inherits Controller
-
-        Private _comicBookRepository As ComicBookRepository = Nothing
+    Public Class ComicBookController : Inherits Controller
+        Private ReadOnly _comicBookRepository As ComicBookRepository
 
         Public Sub New()
             _comicBookRepository = New ComicBookRepository()
         End Sub
 
         Function Index() As ActionResult
-            Dim comicBooks = _comicBookRepository.GetComicBooks
+            Dim comicBooks = _comicBookRepository.GetComicBooks()
 
             Return View(comicBooks)
         End Function
 
-        Function Detail(ByVal id As Integer?) As ActionResult
-            If id Is Nothing Then
+        Function Detail(id As Integer?) As ActionResult
+            If Not id.HasValue Then
                 Return HttpNotFound()
             End If
 
@@ -27,6 +25,5 @@ Namespace Controllers
 
             Return View(comicBook)
         End Function
-
     End Class
 End Namespace
